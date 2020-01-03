69a278f3a6e7c3d6d368d22bb212eab8e6e107cb
Marek Fořt <marekfort@me.com>
2020-01-03 12:17:57 +0100

    Bump version.

--- tezosgen.rb
+++ tezosgen.rb
@@ -1,7 +1,7 @@
 class Tezosgen < Formula
     desc "Generate swift code from smart contract specification"
     homepage "https://github.com/AckeeCZ/tezosgen"
-    url "https://github.com/AckeeCZ/tezosgen/releases/download/1.0.3/tezosgen-1.0.3.zip"
+    url "https://github.com/AckeeCZ/tezosgen/releases/download/1.1.1/tezosgen-1.1.1.zip"
     sha256 "3870b75f14dd943187a388d65151a5c42e89c20659e327f4a139a6f3ddbce852"
     head "https://github.com/AckeeCZ/tezosgen.git"
 
