6af11de9dbb21d71c5503c6c5fafcb1b10c727de
Marek Fořt <marekfort@me.com>
2020-01-03 12:20:08 +0100

    Update checksum.

--- tezosgen.rb
+++ tezosgen.rb
@@ -2,7 +2,7 @@ class Tezosgen < Formula
     desc "Generate swift code from smart contract specification"
     homepage "https://github.com/AckeeCZ/tezosgen"
     url "https://github.com/AckeeCZ/tezosgen/releases/download/1.1.1/tezosgen-1.1.1.zip"
-    sha256 "3870b75f14dd943187a388d65151a5c42e89c20659e327f4a139a6f3ddbce852"
+    sha256 "58924fece1771736aeb8fbf5e6df0389a01e1a323b6fe5a2c3a077a31623eb9a"
     head "https://github.com/AckeeCZ/tezosgen.git"
 
     depends_on :xcode => ["8.0", :build]
